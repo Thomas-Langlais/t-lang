1b5c1e300abc960fd76d074302437bf81f45fdcc
Thomas L'Anglais <thomas1446@gmail.com>
2022-04-26 18:35:57 +0000

    update repl loop to allow new lines

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io::{self, BufRead, Write};
 
 // this is a form of inport in JS
-use core::interpreter::{ExecutionContext, Interpret, SymbolTable, SymbolEntry, SymbolValue};
+use core::interpreter::{ExecutionContext, Interpret, SymbolEntry, SymbolTable, SymbolValue};
 use core::lexer::Lexer;
 use core::parser::Parser;
 
@@ -11,25 +11,45 @@ fn main() {
     print!("--------------------------------------\n");
 
     let global_symbol_table = SymbolTable::new(HashMap::from([
-        (String::from("true"), SymbolEntry {
-            value: SymbolValue::Bool(true),
-            is_constant: true,
-        }),
-        (String::from("false"), SymbolEntry {
-            value: SymbolValue::Bool(false),
-            is_constant: true,
-        })
+        (
+            String::from("true"),
+            SymbolEntry {
+                value: SymbolValue::Bool(true),
+                is_constant: true,
+            },
+        ),
+        (
+            String::from("false"),
+            SymbolEntry {
+                value: SymbolValue::Bool(false),
+                is_constant: true,
+            },
+        ),
     ]));
 
-    loop {
+    'repl: loop {
         print!("> ");
         io::stdout().flush().unwrap();
         let stdin = io::stdin();
         let mut handle = stdin.lock();
         // can only handle one line at a time right now
-        let ref mut buffer: Vec<u8> = Vec::new();
+        let mut buffer: Vec<u8> = Vec::new();
 
-        handle.read_until(b'\n', buffer).unwrap();
+        'read: loop {
+            let bytes = handle.read_until(b'\n', &mut buffer).unwrap();
+            if bytes <= 1 && buffer.len() == bytes {
+                // restart the repl loop when only a newline is in the buffer
+                continue 'repl;
+            } else if bytes == 1 {
+                // allow newline spacing
+                continue 'read;
+            }
+
+            if bytes >= 2 && buffer[buffer.len() - 2] != b';' {
+                // leave if there was no line termination char
+                break 'read;
+            }
+        }
 
         // set the io handle for reading the stream
         let mut reader = Lexer::new(&buffer);

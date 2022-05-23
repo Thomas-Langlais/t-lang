527c2c2c85a08c59dcc31fa8c29da986b01c4f9d
Thomas L'Anglais <thomas1446@gmail.com>
2022-05-23 15:38:36 +0000

    Updated the eval mod to use the ast value

--- core/src/eval.rs
+++ core/src/eval.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Div, Mul, Sub};
 
+use crate::ast::Value;
 use crate::lexer::{CompType, LogicType};
 use crate::exec::{InterpretedType, RTError, Result, Machine};
 
@@ -8,29 +9,29 @@ impl Add for InterpretedType {
 
     fn add(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (InterpretedType::Int(l), InterpretedType::Int(r)) => InterpretedType::Int(l + r),
-            (InterpretedType::Float(l), InterpretedType::Float(r)) => InterpretedType::Float(l + r),
-            (InterpretedType::Int(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(l as f64 + r)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Int(r))) => InterpretedType::Value(Value::Int(l + r)),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Float(r))) => InterpretedType::Value(Value::Float(l + r)),
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(l as f64 + r))
             }
-            (InterpretedType::Float(l), InterpretedType::Int(r)) => {
-                InterpretedType::Float(l + r as f64)
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Float(l + r as f64))
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l as i64 + r as i64)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 + r as i64))
             }
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => {
-                InterpretedType::Int(l as i64 + r)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 + r))
             }
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(if l { 1.0f64 + r } else { r })
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(if l { 1.0f64 + r } else { r }))
             }
-            (InterpretedType::Int(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l + r as i64)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l + r as i64))
             }
-            (InterpretedType::Float(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Float(if r { l + 1.0f64 } else { l })
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Float(if r { l + 1.0f64 } else { l }))
             }
             (InterpretedType::Break, _)
             | (_, InterpretedType::Break)
@@ -47,29 +48,29 @@ impl Sub for InterpretedType {
 
     fn sub(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (InterpretedType::Int(l), InterpretedType::Int(r)) => InterpretedType::Int(l - r),
-            (InterpretedType::Float(l), InterpretedType::Float(r)) => InterpretedType::Float(l - r),
-            (InterpretedType::Int(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(l as f64 - r)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Int(r))) => InterpretedType::Value(Value::Int(l - r)),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Float(r))) => InterpretedType::Value(Value::Float(l - r)),
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(l as f64 - r))
             }
-            (InterpretedType::Float(l), InterpretedType::Int(r)) => {
-                InterpretedType::Float(l - r as f64)
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Float(l - r as f64))
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l as i64 - r as i64)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 - r as i64))
             }
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => {
-                InterpretedType::Int(l as i64 - r)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 - r))
             }
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(if l { 1.0f64 - r } else { -r })
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(if l { 1.0f64 - r } else { -r }))
             }
-            (InterpretedType::Int(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l - r as i64)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l - r as i64))
             }
-            (InterpretedType::Float(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Float(if r { l - 1.0f64 } else { l })
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Float(if r { l - 1.0f64 } else { l }))
             }
             (InterpretedType::Break, _)
             | (_, InterpretedType::Break)
@@ -86,29 +87,29 @@ impl Mul for InterpretedType {
 
     fn mul(self, rhs: Self) -> Self::Output {
         match (self, rhs) {
-            (InterpretedType::Int(l), InterpretedType::Int(r)) => InterpretedType::Int(l * r),
-            (InterpretedType::Float(l), InterpretedType::Float(r)) => InterpretedType::Float(l * r),
-            (InterpretedType::Int(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(l as f64 * r)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Int(r))) => InterpretedType::Value(Value::Int(l * r)),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Float(r))) => InterpretedType::Value(Value::Float(l * r)),
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(l as f64 * r))
             }
-            (InterpretedType::Float(l), InterpretedType::Int(r)) => {
-                InterpretedType::Float(l * r as f64)
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Float(l * r as f64))
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l as i64 * r as i64)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 * r as i64))
             }
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => {
-                InterpretedType::Int(l as i64 * r)
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => {
+                InterpretedType::Value(Value::Int(l as i64 * r))
             }
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
-                InterpretedType::Float(if l { r } else { 0.0 })
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
+                InterpretedType::Value(Value::Float(if l { r } else { 0.0 }))
             }
-            (InterpretedType::Int(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Int(l * r as i64)
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Int(l * r as i64))
             }
-            (InterpretedType::Float(l), InterpretedType::Bool(r)) => {
-                InterpretedType::Float(if r { l } else { 0.0 })
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(r))) => {
+                InterpretedType::Value(Value::Float(if r { l } else { 0.0 }))
             }
             (InterpretedType::Break, _)
             | (_, InterpretedType::Break)
@@ -132,29 +133,29 @@ impl Div for InterpretedType {
         }
 
         match (self, rhs) {
-            (InterpretedType::Int(l), InterpretedType::Int(r)) => Ok(InterpretedType::Int(l / r)),
-            (InterpretedType::Float(l), InterpretedType::Float(r)) => {
-                Ok(InterpretedType::Float(l / r))
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Int(r))) => Ok(InterpretedType::Value(Value::Int(l / r))),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Float(r))) => {
+                Ok(InterpretedType::Value(Value::Float(l / r)))
             }
-            (InterpretedType::Int(l), InterpretedType::Float(r)) => {
-                Ok(InterpretedType::Float(l as f64 / r))
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Float(r))) => {
+                Ok(InterpretedType::Value(Value::Float(l as f64 / r)))
             }
-            (InterpretedType::Float(l), InterpretedType::Int(r)) => {
-                Ok(InterpretedType::Float(l / r as f64))
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Int(r))) => {
+                Ok(InterpretedType::Value(Value::Float(l / r as f64)))
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => {
-                Ok(InterpretedType::Int(l as i64 / r as i64))
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => {
+                Ok(InterpretedType::Value(Value::Int(l as i64 / r as i64)))
             }
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => {
-                Ok(InterpretedType::Int(l as i64 / r))
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => {
+                Ok(InterpretedType::Value(Value::Int(l as i64 / r)))
             }
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
-                Ok(InterpretedType::Float(if l { 1.0f64 / r } else { 0.0 }))
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
+                Ok(InterpretedType::Value(Value::Float(if l { 1.0f64 / r } else { 0.0 })))
             }
             // it will always be the left side, because we handle div by zero above
-            (InterpretedType::Int(l), InterpretedType::Bool(_)) => Ok(InterpretedType::Int(l)),
-            (InterpretedType::Float(l), InterpretedType::Bool(_)) => Ok(InterpretedType::Float(l)),
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(_))) => Ok(InterpretedType::Value(Value::Int(l))),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(_))) => Ok(InterpretedType::Value(Value::Float(l))),
             (InterpretedType::Break, _)
             | (_, InterpretedType::Break)
             | (InterpretedType::Continue, _)
@@ -168,9 +169,9 @@ impl Div for InterpretedType {
 impl InterpretedType {
     fn is_zero(&self) -> bool {
         match self {
-            Self::Float(n) => n == &0.0,
-            Self::Int(n) => n == &0,
-            Self::Bool(n) => !n,
+            Self::Value(Value::Float(n)) => n == &0.0,
+            Self::Value(Value::Int(n)) => n == &0,
+            Self::Value(Value::Bool(n)) => !n,
             Self::Break | Self::Continue => {
                 unreachable!("break, and continues are never to be handledx")
             }
@@ -181,20 +182,20 @@ impl InterpretedType {
 impl PartialEq for InterpretedType {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Self::Int(n1), Self::Int(n2)) => n1 == n2,
-            (Self::Float(n1), Self::Float(n2)) => n1 == n2,
+            (Self::Value(Value::Int(n1)), Self::Value(Value::Int(n2))) => n1 == n2,
+            (Self::Value(Value::Float(n1)), Self::Value(Value::Float(n2))) => n1 == n2,
             // because to_int_unchecked removes the float fraction representation,
             // it must ensure the float is it's ceiling before comparing
-            (Self::Int(n1), Self::Float(n2)) => {
+            (Self::Value(Value::Int(n1)), Self::Value(Value::Float(n2))) => {
                 n1 == &unsafe { n2.ceil().to_int_unchecked::<i64>() }
             }
-            (Self::Float(n1), Self::Int(n2)) => {
+            (Self::Value(Value::Float(n1)), Self::Value(Value::Int(n2))) => {
                 &(unsafe { n1.ceil().to_int_unchecked::<i64>() }) == n2
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => l == r,
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => *l as i64 == *r,
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => l == r,
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => *l as i64 == *r,
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
                 *r == {
                     if *l {
                         1.0f64
@@ -203,8 +204,8 @@ impl PartialEq for InterpretedType {
                     }
                 }
             }
-            (InterpretedType::Int(l), InterpretedType::Bool(r)) => *l == *r as i64,
-            (InterpretedType::Float(l), InterpretedType::Bool(r)) => {
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(r))) => *l == *r as i64,
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(r))) => {
                 *l == {
                     if *r {
                         1.0f64
@@ -226,24 +227,24 @@ impl PartialEq for InterpretedType {
 impl PartialOrd for InterpretedType {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match (self, other) {
-            (Self::Int(n1), Self::Int(n2)) => n1.partial_cmp(n2),
-            (Self::Float(n1), Self::Float(n2)) => n1.partial_cmp(n2),
+            (Self::Value(Value::Int(n1)), Self::Value(Value::Int(n2))) => n1.partial_cmp(n2),
+            (Self::Value(Value::Float(n1)), Self::Value(Value::Float(n2))) => n1.partial_cmp(n2),
             // because to_int_unchecked removes the float fraction representation,
             // it must ensure the float is it's ceiling before comparing
-            (Self::Int(n1), Self::Float(n2)) => {
+            (Self::Value(Value::Int(n1)), Self::Value(Value::Float(n2))) => {
                 let ceiled = n2.ceil();
                 let i2 = &unsafe { ceiled.to_int_unchecked::<i64>() };
                 n1.partial_cmp(i2)
             }
-            (Self::Float(n1), Self::Int(n2)) => {
+            (Self::Value(Value::Float(n1)), Self::Value(Value::Int(n2))) => {
                 let ceiled = n1.ceil();
                 let i1 = &unsafe { ceiled.to_int_unchecked::<i64>() };
                 i1.partial_cmp(n2)
             }
             // boolean handling
-            (InterpretedType::Bool(l), InterpretedType::Bool(r)) => l.partial_cmp(r),
-            (InterpretedType::Bool(l), InterpretedType::Int(r)) => (*l as i64).partial_cmp(r),
-            (InterpretedType::Bool(l), InterpretedType::Float(r)) => {
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Bool(r))) => l.partial_cmp(r),
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Int(r))) => (*l as i64).partial_cmp(r),
+            (InterpretedType::Value(Value::Bool(l)), InterpretedType::Value(Value::Float(r))) => {
                 if *l {
                     1.0f64
                 } else {
@@ -251,8 +252,8 @@ impl PartialOrd for InterpretedType {
                 }
             }
             .partial_cmp(r),
-            (InterpretedType::Int(l), InterpretedType::Bool(r)) => l.partial_cmp(&(*r as i64)),
-            (InterpretedType::Float(l), InterpretedType::Bool(r)) => l.partial_cmp({
+            (InterpretedType::Value(Value::Int(l)), InterpretedType::Value(Value::Bool(r))) => l.partial_cmp(&(*r as i64)),
+            (InterpretedType::Value(Value::Float(l)), InterpretedType::Value(Value::Bool(r))) => l.partial_cmp({
                 if *r {
                     &1.0f64
                 } else {
@@ -275,7 +276,7 @@ impl Machine<'_> {
             '+' => input,
             '-' => {
                 let rhs = input?;
-                Ok(InterpretedType::Int(-1) * rhs)
+                Ok(InterpretedType::Value(Value::Int(-1)) * rhs)
             }
             _ => unreachable!("A unary arithmetic operation can only be -/+"),
         }
@@ -285,7 +286,7 @@ impl Machine<'_> {
         match lgc_type {
             LogicType::NOT => {
                 let rhs = input?;
-                Ok(InterpretedType::Bool(!bool::from(rhs)))
+                Ok(InterpretedType::Value(Value::Bool(!bool::from(rhs))))
             }
             _ => unreachable!("A unary operator can only be !"),
         }
@@ -308,19 +309,19 @@ impl Machine<'_> {
         rhs: InterpretedType,
     ) -> Result {
         match cmp_type {
-            CompType::EE => Ok(InterpretedType::Bool(lhs == rhs)),
-            CompType::NE => Ok(InterpretedType::Bool(lhs != rhs)),
-            CompType::GT => Ok(InterpretedType::Bool(lhs > rhs)),
-            CompType::GTE => Ok(InterpretedType::Bool(lhs >= rhs)),
-            CompType::LT => Ok(InterpretedType::Bool(lhs < rhs)),
-            CompType::LTE => Ok(InterpretedType::Bool(lhs <= rhs)),
+            CompType::EE => Ok(InterpretedType::Value(Value::Bool(lhs == rhs))),
+            CompType::NE => Ok(InterpretedType::Value(Value::Bool(lhs != rhs))),
+            CompType::GT => Ok(InterpretedType::Value(Value::Bool(lhs > rhs))),
+            CompType::GTE => Ok(InterpretedType::Value(Value::Bool(lhs >= rhs))),
+            CompType::LT => Ok(InterpretedType::Value(Value::Bool(lhs < rhs))),
+            CompType::LTE => Ok(InterpretedType::Value(Value::Bool(lhs <= rhs))),
         }
     }
 
     pub(crate) async fn term_logic_op(&self, lgc_type: LogicType, lhs: bool, rhs: bool) -> Result {
         match lgc_type {
-            LogicType::AND => Ok(InterpretedType::Bool(lhs && rhs)),
-            LogicType::OR => Ok(InterpretedType::Bool(lhs || rhs)),
+            LogicType::AND => Ok(InterpretedType::Value(Value::Bool(lhs && rhs))),
+            LogicType::OR => Ok(InterpretedType::Value(Value::Bool(lhs || rhs))),
             _ => unreachable!("Cannot interpret ! operation for a term"),
         }
     }
